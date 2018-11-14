7a642fa597a168aeac13ff78665e83ecf0b4e2e9
davidepatruno <patrunodavid@gmail.com>
2018-11-14 16:29:11 +0100

    else problem

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ var db_email = ["pippo@ex.it", "franco@msn.com", "checco@mail.es"];
 var user_mail = prompt("inserisci la tua mail");
 document.getElementById('user_mail').innerHTML = user_mail;
 
-if (db_email = user_mail) {
-  document.writeln ('la tua mail è corretta');
+for (var i = 0; i < db_email.length; i++ ) {
+  if (user_mail == db_email[i]) {
+    document.writeln ('Sei nella lista');
+  }
+  else {
+    document.writeln ('non sei nella lista');
+  }
 }
